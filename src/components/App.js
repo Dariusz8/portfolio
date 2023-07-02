aa51252b19f7b71edba4851a12c2bd84ebc19bd2
Dariusz Czeczuk <dariusz.czeczuk@hotmail.com>
2023-07-02 15:56:16 -0400

    portfolio - setup

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div class="container-fluid">
+      <h1>hello</h1>
+      <p>this is a</p>
+    </div>
+  );
+}
+
+export default App;
