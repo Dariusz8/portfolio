56dc82fd237da8d27f272071f7cd837b955c15ab
Dariusz Czeczuk <dariusz.czeczuk@hotmail.com>
2023-07-11 18:12:07 -0400

    portfolio - Bodytext

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const About = () => {
+    return(
+        <AboutText>
+            As a passionate and versatile professional with a B.Sc in Cell and Molecular Biology and recent successful completion of a bootcamp in full-stack web development, I am excited to transition into a field that offers constant new tasks, challenges and opportunities for growth. Through my recent work as a lab technician, i honed my attention to detail, multitasking and working effectively in a team-based environment. Seeing the potential of emerging technology in various industries, I pursued a bootcamp in web development. This allowed me to acquire proficiency in JavaScript, React, HTML, CSS, Node.js and MongoDB gaining experience in both front-end and back-end development. I look forward to contributing my diverse skill set and passion for science and technology to a team that values continual learning and the pursuit of helpful outcomes for others.
+        </AboutText>
+    )
+}
+const AboutText = styled.div`
+`
+
+export default About
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,24 @@
 import React from "react";
 import styled from "styled-components";
+import About from "./About";
 
 
 const Body = () => {
     return(
-        <div>
-        </div>
+        <WholeContent>
+            <About/>
+        </WholeContent>
     )
 }
+
+const WholeContent = styled.div`
+width:100vw;
+height:87vh;
+border: solid 5px purple;
+position: fixed;
+top:13vh;
+display:flex;
+align-items: center;
+`
 
 export default Body;
