9bfcee6ea267bea89d3291e0e91741825dfa80e8
Dariusz Czeczuk <dariusz.czeczuk@hotmail.com>
2023-07-12 20:38:52 -0400

    portfolio - splitAbout

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,13 @@
 import React from "react";
 import styled from "styled-components";
 import About from "./About";
+import { useState } from "react";
 
 
 const Body = () => {
+    const [AboutActive, setAboutActive] = useState(true);
+    const [ProjectsActive, setProjectsActive] = useState(false);
+    const [SkillsActive, setSkillsActive] = useState(false);
     return(
         <DaBody>
         <ButtonsRow>
@@ -12,7 +16,9 @@ const Body = () => {
             <SkillsButton>Skills</SkillsButton>
         </ButtonsRow>
         <WholeContent>
-            <About/>
+            <AboutSection>
+                <About/>
+            </AboutSection>
         </WholeContent>
         </DaBody>
     )
@@ -38,15 +44,29 @@ justify-content: space-evenly;
 //border: solid 5px orange;
 `
 const AboutButton = styled.button`
+background-color:transparent;
+font-size:28px;
 border:none;
+`
+const AboutSection = styled.div`
+width:50vw;
+height:87vh;
+//border: solid 5px purple;
+position: fixed;
+top:13vh;
+left:50vw;
+display:flex;
+align-items: center;
 `
 const ProjectsButton = styled.button`
+background-color:transparent;
+font-size:28px;
 border:none;
-
 `
 const SkillsButton = styled.button`
+background-color:transparent;
+font-size:28px;
 border:none;
-
 `
 
 export default Body;
