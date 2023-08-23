57bc25a5f924c544fe05231dfa220fb478e4599f
Dariusz Czeczuk <dariusz.czeczuk@hotmail.com>
2023-08-22 20:47:30 -0400

    portfolio - contextSetUp

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Header from "./Header";
 import Body from "./Body";
+import {ThemeContextProvider} from "./ThemeContext";
 
 function App() {
   return (
-    <div>
+    <ThemeContextProvider >
     <Header/>
     <Body/>
-    </div>
+    </ThemeContextProvider>
   );
 }
 export default App;
