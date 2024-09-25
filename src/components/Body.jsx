c611dfe08f93478cb6b4fa699f671f03094bc668
Dariusz Czeczuk <dariusz.czeczuk@hotmail.com>
2024-09-25 18:19:36 -0400

    darkFontFixHeadings

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -29,57 +29,133 @@ const Body = () => {
         setProjectsActive(false);
         setSkillsActive(true);
     };
-
+//TODO the headings not being white in dark mode + (take off animation)
     const EngHeadings = () => 
         <div className="ButtonsRow" >
-            <div className="AboutButton"  onMouseDown={handleAboutClick}>
+            {
+                !nightOn &&
+                <>
+                <div className="AboutButton"  onMouseDown={handleAboutClick}>
                 About
-            </div>
-            <div className="ProjectsButton" onMouseDown={handleProjectsClick}>
+                </div>
+                <div className="ProjectsButton" onMouseDown={handleProjectsClick}>
                 Projects
-            </div>
-            <div className="SkillsButton"  onMouseDown={handleSkillsClick}>
+                </div>
+                <div className="SkillsButton"  onMouseDown={handleSkillsClick}>
                 Skills
-            </div>
+                </div></>
+            }
+            
+            {
+                nightOn &&
+                <>
+                <div className="NightAboutButton"  onMouseDown={handleAboutClick}>
+                About
+                </div>
+                <div className="NightProjectsButton" onMouseDown={handleProjectsClick}>
+                Projects
+                </div>
+                <div className="NightSkillsButton"  onMouseDown={handleSkillsClick}>
+                Skills
+                </div></>
+            }
         </div>
 
     const FrHeadings = () => 
         <div className="ButtonsRow" >
-            <div className="AboutButton"  onMouseDown={handleAboutClick}>
-                À propos
-            </div>
-            <div className="ProjectsButton" onMouseDown={handleProjectsClick}>
-                Projets
-            </div>
-            <div className="SkillsButton"  onMouseDown={handleSkillsClick}>
+            {
+                !nightOn &&
+                <>
+                <div className="AboutButton"  onMouseDown={handleAboutClick}>
+                    À propos
+                </div>
+                <div className="ProjectsButton" onMouseDown={handleProjectsClick}>
+                    Projets
+                </div>
+                <div className="SkillsButton"  onMouseDown={handleSkillsClick}>
                 Compétences
-            </div>
+                </div>
+                </>
+            }
+            {
+                nightOn &&
+                <>
+                <div className="NightAboutButton"  onMouseDown={handleAboutClick}>
+                    À propos
+                </div>
+                <div className="NightProjectsButton" onMouseDown={handleProjectsClick}>
+                    Projets
+                </div>
+                <div className="NightSkillsButton"  onMouseDown={handleSkillsClick}>
+                    Compétences
+                </div>
+                </>
+            }
+            
         </div>
 
     const PlHeadings = () => 
         <div className="ButtonsRow" >
-            <div className="AboutButton"  onMouseDown={handleAboutClick}>
-                O mnie
-            </div>
-            <div className="ProjectsButton" onMouseDown={handleProjectsClick}>
-                Projekty
-            </div>
-            <div className="SkillsButton"  onMouseDown={handleSkillsClick}>
-                Umiejętności
-            </div>
+            {
+                !nightOn &&
+                <>
+                <div className="AboutButton"  onMouseDown={handleAboutClick}>
+                    O mnie
+                </div>
+                <div className="ProjectsButton" onMouseDown={handleProjectsClick}>
+                    Projekty
+                </div>
+                <div className="SkillsButton"  onMouseDown={handleSkillsClick}>
+                    Umiejętności
+                </div>
+                </>
+            }
+            {
+                nightOn &&
+                <>
+                <div className="NightAboutButton"  onMouseDown={handleAboutClick}>
+                    O mnie
+                </div>
+                <div className="NightProjectsButton" onMouseDown={handleProjectsClick}>
+                    Projekty
+                </div>
+                <div className="NightSkillsButton"  onMouseDown={handleSkillsClick}>
+                    Umiejętności
+                </div>
+                </>
+            }
         </div>
 
     const EspHeadings = () => 
         <div className="ButtonsRow" >
-            <div className="AboutButton"  onMouseDown={handleAboutClick}>
-                Acerca de
-            </div>
-            <div className="ProjectsButton" onMouseDown={handleProjectsClick}>
-                Proyectos
-            </div>
-            <div className="SkillsButton"  onMouseDown={handleSkillsClick}>
-                Habilidades
-            </div>
+            {
+                !nightOn &&
+                <>
+                <div className="AboutButton"  onMouseDown={handleAboutClick}>
+                    Acerca de
+                </div>
+                <div className="ProjectsButton" onMouseDown={handleProjectsClick}>
+                    Proyectos
+                </div>
+                <div className="SkillsButton"  onMouseDown={handleSkillsClick}>
+                    Habilidades
+                </div>
+                </>
+            }
+            {
+                nightOn &&
+                <>
+                <div className="NightAboutButton"  onMouseDown={handleAboutClick}>
+                    Acerca de
+                </div>
+                <div className="NightProjectsButton" onMouseDown={handleProjectsClick}>
+                    Proyectos
+                </div>
+                <div className="NightSkillsButton"  onMouseDown={handleSkillsClick}>
+                    Habilidades
+                </div>
+                </>
+            }
         </div>
 
     return(
