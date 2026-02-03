dfbf76486c41e9dbcb54f87ef436f853440c887b
Dariusz Czeczuk <dariusz.czeczuk@hotmail.com>
2026-02-03 14:39:09 -0500

    rustyTest

---
+++ src/components/MoreMe.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import '../MoreMe.css';
+
+const MoreMe = () => {
+
+    const handleBiobankingClick = () =>{
+        
+    }
+
+    const handleSOPClick = () =>{
+        
+    }
+
+    const handleViabilityClick = () =>{
+        
+    }
+    return(
+        <>
+        <h2>Articles of interest</h2>
+        <div>
+            Biobanking
+        </div>
+        <div>
+            SOP
+        </div>
+        <div>
+            Viability
+        </div>
+        </>
+    )
+}
+
+export default MoreMe
